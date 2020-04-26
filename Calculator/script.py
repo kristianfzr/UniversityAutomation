5f595a549a3846b7f63c2ae7c711b8989c7de533
Kristian Kanchev <kristianfzr@gmail.com>
2020-04-26 16:45:54 +0300

    Changed directory for site navigation.

--- Calculator/script.py
+++ Calculator/script.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 chromedriver = "D:\Scripts\chromedriver"
 driver = webdriver.Chrome(chromedriver)
-driver.get("http://localhost:63342/UniversityAutomation/calculator.html")
+driver.get("http://localhost:63342/UniversityAutomation/Calculator/calculator.html")
 
 # Click number 1
 elem = driver.find_element_by_xpath("//input[@value='1']")
@@ -31,6 +31,5 @@ if res == "3":
     print("Test passed, the result is: ", res)
 else:
     print("Test did not pass, the result is: ", res)
-
 
 driver.close()
